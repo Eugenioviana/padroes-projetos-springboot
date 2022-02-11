b7ce100a0c6192810e706c8a97c8dbb1b10a3b1e
Eugenio oliveira <94003926+Eugenioviana@users.noreply.github.com>
2022-02-11 14:44:43 -0300

    Update ViaCepService.java

--- src/main/java/one/digitalinnovatio/springboot/gof/service/ViaCepService.java
+++ src/main/java/one/digitalinnovatio/springboot/gof/service/ViaCepService.java
@@ -1,20 +1,11 @@
-package one.digitalinnovation.gof.service;
+package one.digitalinnovatio.springboot.gof.springBoot.gof.service;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import one.digitalinnovation.gof.model.Endereco;
+import one.digitalinnovatio.springboot.gof.springBoot.gof.model.Endereco;
 
-/**
- * Client HTTP, criado via <b>OpenFeign</b>, para o consumo da API do
- * <b>ViaCEP</b>.
- * 
- * @see <a href="https://spring.io/projects/spring-cloud-openfeign">Spring Cloud OpenFeign</a>
- * @see <a href="https://viacep.com.br">ViaCEP</a>
- * 
- * @author falvojr
- */
 @FeignClient(name = "viacep", url = "https://viacep.com.br/ws")
 public interface ViaCepService {
 
