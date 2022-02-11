e32e7e623959e46c3937fd8a4854d6050d4bb331
Eugenioviana <oliveiraeugenio511@gmail.com>
2022-02-11 12:44:54 -0300

    Premeiro commit

---
+++ src/test/java/one/digitalinnovatio/springboot/gof/springBoot/gof/SpringBootGofApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovatio.springboot.gof.springBoot.gof;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGofApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
